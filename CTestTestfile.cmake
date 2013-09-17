32a4e2014492bcb24a1df37c85a4af86e5fa2a66
Francesco Delfino <delfinof@gmail.com>
2013-09-17 09:56:49 +0200

    visual studio 2012 configuration file added (generated with cmake...)

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Programming/My_MxM_stuff/zlib-1.2.8
+# Build directory: C:/Programming/My_MxM_stuff/zlib-1.2.8
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(example "example")
